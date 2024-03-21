e6fe2e82fb5b9d196200afdd2284976d006df260
Thomas Lagies <thomas.lagies@sealsystems.de>
2024-03-21 13:54:51 +0100

    removed auto-session, toggle neo-tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,6 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>t", ":Neotree filesystem reveal right<CR>", {})
+		vim.keymap.set("n", "<leader>t", ":Neotree toggle right filesystem<CR>", {})
 	end,
 }
