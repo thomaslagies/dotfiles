177932ffac24795324ac4ab5aaeebd31a008ee90
Thomas Lagies <thomas.lagies@sealsystems.de>
2024-08-21 15:58:03 +0200

    feat: update key bindings and add new plugins- Updated key bindings in `.config/aerospace/aerospace.toml` for better usability.- Added new key bindings for layout management and configuration reloading.- Removed unnecessary workspace bindings to streamline configuration.- Added `CopilotChat.nvim` plugin configuration in `.config/nvim/lua/plugins/copilot-chat.lua`.- Updated `lazy-lock.json` to include new plugin versions and dependencies.- Added `vim-surround` plugin configuration in `.config/nvim/lua/plugins/vim-surround.lua`.- Updated `.config/tmux/tmux.conf` to change terminal settings and improve status bar configuration.- Added new configuration file for `wezterm` terminal emulator in `.config/wezterm/wezterm.lua`.BREAKING CHANGE: Removed several workspace bindings which may affect existing workflows.

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font_size = 20.0
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.keys = {}
+
+config.audible_bell = "Disabled"
+
+return config
