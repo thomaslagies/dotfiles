bab61d4396f3a640780220d72e3a6c2a3c3a15e7
Thomas Lagies <thomas.lagies@sealsystems.de>
2026-06-23 13:12:07 +0200

    get rid of old stuff, byeee

--- .config/nvim.old/lua/plugins/comment.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"terrortylor/nvim-comment",
-	config = function()
-		require("nvim_comment").setup()
-	end,
-}
--- .config/nvim.old/lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"github/copilot.vim",
-	config = function()
-		vim.cmd([[imap <silent><expr> <C-a> copilot#Accept("\<CR>")]])
-		vim.cmd([[imap <silent><expr> <C-n> copilot#Next()]])
-    vim.g.copilot_settings = { selectedCompletionModel = 'gpt-5-copilot' }
-    vim.g.copilot_integration_id = "vscode-chat"
-	end,
-}
