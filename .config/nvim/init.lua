1b4184a7cb011caacecb68742450f1bb614160af
Thomas Lagies <thomas.lagies@sealsystems.de>
2026-08-21 10:19:35 +0200

    changed key

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -70,7 +70,7 @@ require('which-key').setup {
 	icons = { mappings = vim.g.have_nerd_font },
 	-- Document existing key chains
 	spec = {
-		{ '<leader>s', group = '[S]earch',    mode = { 'n', 'v' } },
+		{ '<leader>ll', group = '[S]earch',    mode = { 'n', 'v' } },
 		{ '<leader>t', group = '[T]oggle' },
 		{ '<leader>h', group = 'Git [H]unk',  mode = { 'n', 'v' } },  -- Enable gitsigns recommended keymaps first
 		{ 'gr',        group = 'LSP Actions', mode = { 'n' } },
